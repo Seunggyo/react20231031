73108e4778d6f0d3612b17d43fb2a83ea49832ef
Seunggyo <tmdry99@gmail.com>
2023-10-31 12:21:08 +0900

    component 연습 후 작성완료 후 변경

--- src/App.js
+++ src/App.js
@@ -18,26 +18,11 @@ function MyComponent1() {
 function MyComponent2() {
   return (
       <>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-          aspernatur consequatur ipsa necessitatibus sed sunt. Accusamus cumque
-          ducimus eligendi error harum illo ipsam perspiciatis quae, quibusdam
-          sint temporibus ut voluptatum.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
         <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad
-            blanditiis dolore dolorem ex explicabo illo in labore maiores non
-            officia repellat, sit voluptas! Ab optio recusandae repellat. In,
-            neque!
-          </li>
-          <li>Atque dicta eos pariatur tempore. Ab ad adipisci consequatur,
-            dignissimos dolore eius eligendi ex facere fugit libero nulla
-            numquam odit omnis pariatur repellat reprehenderit sapiente ut
-            veniam vero voluptatum? Rerum?
-          </li>
-          <li>A ad aliquam blanditiis consectetur consequatur cupiditate
-            delectus deleniti eius enim excepturi exercitationem explicabo,
-            fugit ipsum iste minima modi obcaecati quasi quisquam quod saepe
-            suscipit vel voluptate. Incidunt nihil, veniam.
-          </li>
+          <li>Lorem.</li>
+          <li>Expedita.</li>
+          <li>Blanditiis.</li>
         </ul>
       </>
   );

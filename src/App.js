7a4ed346add68de54af4623c2b93228c1abf933c
Seunggyo <tmdry99@gmail.com>
2023-10-31 11:40:09 +0900

    component, 주석 처리 하는법 , jsx 에 대한 내용들

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-
-
-
 function App() {
+  // component : html을 감싸고 있는 함수
   return (
-    <div>
+      <div>
+        <h1>hello react</h1>
+        <h2>HI REACT</h2>
+        <MyComp/>
+      </div>
+  );
+}
 
-    </div>
+// built-in component 는 소 문자로 시작
+// component 명을 작성할 때는 항상 대 문자로 시작
+// return 문을 가져야 함
+// return 값은 보통 JSX 코드
+
+function MyComp() {
+  // return 에 jsx 코드 작성 가능
+  // 꼭 하나의 root tag 가 존재 하여야 한다.
+  // return 값이 여러 줄이면 ()로 감싸기
+  // root tag 가 필요 없다면 fragment 를 사용
+  // fragment : <> 안에 아무 것도 안 적혀 있는 코드
+
+  // jsx 에서 주석 사용 법
+  // {/*  */}
+  return (
+      <div>
+        <h1>HI COMPONENT</h1>
+        <h2>hello</h2>
+      </div>
   );
 }
 

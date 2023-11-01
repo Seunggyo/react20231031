a7b486c504a4043435d4bbfa1587882d9b615e09
Seunggyo <tmdry99@gmail.com>
2023-11-01 11:34:01 +0900

    props destructuring assignment 구조분해할당, 기본값 주기 연습 시작후 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
-
-
 function App() {
   return (
     <div>
+      <MyComp m="40px" p="20px" color="white" bg="silver" text="hello world" />
+      <MyComp m="20px" p="5px" color="gold" bg="black" />
+      <MyComp m="10px" color="green" text="hello java" />
+      <MyComp p="30px" />
+      <MyComp />
+    </div>
+  );
+}
 
+function MyComp({
+  m = "30px",
+  p = "10px",
+  color = "black",
+  bg = "gold",
+  text = "hello react",
+}) {
+  return (
+    <div style={{ margin: m, padding: p, color: color, backgroundColor: bg }}>
+      <h1>{text}</h1>
     </div>
   );
 }

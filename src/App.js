039b5fa57b10a7cbcbf7991211a0108f4ff0f2ba
Seunggyo <tmdry99@gmail.com>
2023-11-01 10:46:08 +0900

    props destructuring assignment 구조분해할당 연습 시작 후 종료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-
+function MyElem({ name, age, team }) {
+  return (
+    <>
+      <h1>
+        {name}의 나이는 {age}이고 팀은 {team}이다
+      </h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyElem name={"흥민"} age={44} team={"토트넘"} />
+      <MyElem name={"강인"} age={33} team={"파리"} />
     </div>
   );
 }

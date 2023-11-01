b5eb3eaffdc9443389835329ac0cacd7ce503b42
Seunggyo <tmdry99@gmail.com>
2023-11-01 11:22:36 +0900

    props destructuring assignment 구조분해할당, 기본값 주기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 function App() {
   return (
     <>
-      <MyComp value={"흥민"} address={"신촌"} />
-      <MyComp value={"강인"} address={"강남"} />
+      <MyComp name={"흥민"} address={"신촌"} age={33} city={"서울"} />
+      <MyComp address={"노포"} age={22} city={"부산"} />
+      <MyComp address={"런던"} city={"영국"} />
+      <MyComp name="희찬" age={66} address={"paris"} city={"french"} />
     </>
   );
 }
 
 // {value: "흥민", address: "신촌"
-function MyComp(props) {
-  let { value, address } = props;
+function MyComp({ name = "anonymous", address, city, age = 100 }) {
   return (
     <div>
-      <h1>
-        {value}은(는) {address}에 산다
-      </h1>
+      <p>
+        {name}은(는) {age}세 이고 {address}, {city}에 산다
+      </p>
     </div>
   );
 }

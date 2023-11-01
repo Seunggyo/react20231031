1157841224eb441ebc2fcc3c8fcbc377516afd14
Seunggyo <tmdry99@gmail.com>
2023-11-01 15:53:30 +0900

    보통의 css 사용시 클래스 명이 중복될수 있는경우의 해결 방법

--- src/component/MyContainer.js
+++ src/component/MyContainer.js
@@ -1,9 +1,9 @@
-import "../style/style2.css";
+import css from "../style/style1.module.css";
 
 export function MyContainer() {
   return (
     <div>
-      <h1 className="boxClass">Lorem ipsum dolor sit amet.</h1>
+      <h1 className={css.boxClass}>Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }

962bb55c79d48a70fee0e383d00286f6329bd23f
Seunggyo <tmdry99@gmail.com>
2023-11-01 17:34:35 +0900

    chakra ui 설치

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -3,7 +3,9 @@ import style from "../style/mystyle.module.css";
 export function MyText() {
   return (
     <div>
-      <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={[style.error, style.special, style.coffee].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }

7044f7f586f5a96aaa637bbd59c73a48c4bebf43
Seunggyo <tmdry99@gmail.com>
2023-11-01 14:27:22 +0900

    default, export, import 연습 완료

---
+++ src/component/MyContainer.js
@@ -0,0 +1,12 @@
+function MyContainer() {
+  return (
+    <div>
+      <h1>make box</h1>
+    </div>
+  );
+}
+
+export const person = { name: "ksg", address: "ulsan" };
+export const team = { title: "kt", score: 4 };
+
+export default MyContainer;

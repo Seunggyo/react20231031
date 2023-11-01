18200247a02aab21d2d10246caa05ce7bd49dc0a
Seunggyo <tmdry99@gmail.com>
2023-11-01 11:13:51 +0900

    props destructuring assignment 구조분해할당 연습 시작 후 종료 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
-
-
 function App() {
   return (
     <div>
+      <MyBox
+        text={"텍스트가 많이 있습니다."}
+        p={0}
+        m={0}
+        color={"green"}
+        bg={"blue"}
+      />
+      <MyBox
+        text={"텍스트가 전보다 많이 있습니다."}
+        p={30}
+        m={30}
+        color={"blue"}
+        bg={"red"}
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, bg, color, text }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
     </div>
   );
 }

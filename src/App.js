bd0c66cada71c240e9dc53f5ea29a8e555baa5de
Seunggyo <tmdry99@gmail.com>
2023-11-01 09:46:11 +0900

    pros작성 활용연습 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
-
+function MyHeader(props) {
+  return (
+    <div style={{ color: props.color, background: props.background }}>
+      <h1>{props.text}</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyHeader color={"white"} background={"black"} text={"내가 만든 제목"} />
+      <MyHeader
+        color={"orange"}
+        background={"skyblue"}
+        text={"다시 만든 제목"}
+      />
     </div>
   );
 }

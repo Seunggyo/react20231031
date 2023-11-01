dfe6086fa0cbf63452090935ff965b90e73bbe67
Seunggyo <tmdry99@gmail.com>
2023-11-01 10:16:01 +0900

    pros작성 활용연습 완료 다시한번

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-
-
 function App() {
   return (
     <div>
+      <MyText
+        style={{ color: "red", backgroundColor: "darkblue", fontSize: "80px" }}
+        value={"asdf"}
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
     </div>
   );
 }

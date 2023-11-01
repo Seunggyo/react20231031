3f13376e1523054666633ed91e85a956787387a4
Seunggyo <tmdry99@gmail.com>
2023-11-01 17:49:26 +0900

    chakra ui 설치 후 여러가지 체험

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { MyText } from "./component/MyText";
 import { MyBlock } from "./component/MyBlock";
+import * as React from "react";
+import { Avatar, ChakraProvider, Kbd } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div>
-      <MyText />
-      <MyBlock />
-    </div>
+    <ChakraProvider>
+      <div>
+        <span>
+          <Kbd>control</Kbd> + <Kbd>C</Kbd>
+        </span>
+        <Avatar name="KSG" />
+        <MyText />
+        <MyBlock />
+      </div>
+    </ChakraProvider>
   );
 }
 

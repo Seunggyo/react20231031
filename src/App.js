857dbae1d0fc0ba1fe6a57a602971f345d2f589b
Seunggyo <tmdry99@gmail.com>
2023-11-02 14:24:29 +0900

    modal의 여러가지 활용방법 2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
   return (
     <>
-      <Button onClick={onOpen}>모달 열기</Button>
-
-      <Modal
-        closeOnOverlayClick={false}
-        isOpen={isOpen}
-        onClose={onClose}
-        isCentered
-        motionPreset="slideInBottom"
+      <Button
+        onClick={() =>
+          toast({
+            title: "제목",
+            description: "내용입니다.",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+            position: "top",
+          })
+        }
       >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>모달의 제목</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda,
-            dolorem est explicabo magni numquam quis repellat! Adipisci aperiam
-            asperiores atque autem, cum debitis deleniti dicta, distinctio
-            laborum, optio quas vel?
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              닫기
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+        토스트 보이기
+      </Button>
+      <Button
+        onClick={() =>
+          toast({
+            title: "제목",
+            description: "부정",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+            position: "top",
+          })
+        }
+      >
+        부정 보이기
+      </Button>
     </>
   );
 }

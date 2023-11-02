a521e1be038d0e103c318d9b1114845db6b71f46
Seunggyo <tmdry99@gmail.com>
2023-11-02 11:28:09 +0900

    simpleGrid 사용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"gray.300"}
-        gap={3}
-        align={"center"}
-        justify={"space-around"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"red"}>Quibusdam.</Box>
-        <Box bg={"gray"}>Ducimus!</Box>
-        <Box bg={"blue"}>Vitae.</Box>
-        <Box bg={"green"}>Quod!</Box>
-      </Flex>
+      <SimpleGrid gap={5} columns={2}>
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Porro?</Box>
+        <Box bg="blue.300">Illum.</Box>
+        <Box bg="blue.300">Totam!</Box>
+        <Box bg="blue.300">Delectus.</Box>
+        <Box bg="blue.300">Fugit?</Box>
+        <Box bg="blue.300">Ducimus?</Box>
+        <Box bg="blue.300">Ab?</Box>
+        <Box bg="blue.300">At?</Box>
+        <Box bg="blue.300">Modi.</Box>
+        <Box bg="blue.300">Esse.</Box>
+        <Box bg="blue.300">Earum?</Box>
+        <Box bg="blue.300">Aut?</Box>
+        <Box bg="blue.300">Cumque!</Box>
+        <Box bg="blue.300">Dolore!</Box>
+        <Box bg="blue.300">Culpa.</Box>
+        <Box bg="blue.300">Veniam?</Box>
+        <Box bg="blue.300">Earum.</Box>
+        <Box bg="blue.300">Impedit!</Box>
+        <Box bg="blue.300">Sapiente!</Box>
+        <Box bg="blue.300">Sapiente?</Box>
+        <Box bg="blue.300">Voluptas?</Box>
+        <Box bg="blue.300">Illo.</Box>
+        <Box bg="blue.300">Ipsam!</Box>
+        <Box bg="blue.300">Quasi.</Box>
+        <Box bg="blue.300">Obcaecati?</Box>
+        <Box bg="blue.300">Similique.</Box>
+        <Box bg="blue.300">Totam.</Box>
+        <Box bg="blue.300">Earum.</Box>
+        <Box bg="blue.300">Temporibus?</Box>
+      </SimpleGrid>
     </>
   );
 }

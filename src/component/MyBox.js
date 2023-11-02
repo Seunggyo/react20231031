5bad8d0002f905756c65d27c616c8a833386842a
Seunggyo <tmdry99@gmail.com>
2023-11-02 09:34:30 +0900

    필요없는 파일들 삭제함

--- src/component/MyBox.js
+++
@@ -1,9 +0,0 @@
-import style from "../style/style2.module.css";
-
-export function MyBox() {
-  return (
-    <div>
-      <h1 className={style.boxClass}>Lorem ipsum dolor sit amet.</h1>
-    </div>
-  );
-}

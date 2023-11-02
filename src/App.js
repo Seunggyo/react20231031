9fbf6d0d51541f93d642e1a3b1bda9afb05ef527
Seunggyo <tmdry99@gmail.com>
2023-11-02 11:41:33 +0900

    card 와 cardbody 사용해봄

--- src/App.js
+++ src/App.js
@@ -1,39 +1,142 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Card, CardBody, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
-        <Box bg="blue.300">Lorem.</Box>
-        <Box bg="blue.300">Porro?</Box>
-        <Box bg="blue.300">Illum.</Box>
-        <Box bg="blue.300">Totam!</Box>
-        <Box bg="blue.300">Delectus.</Box>
-        <Box bg="blue.300">Fugit?</Box>
-        <Box bg="blue.300">Ducimus?</Box>
-        <Box bg="blue.300">Ab?</Box>
-        <Box bg="blue.300">At?</Box>
-        <Box bg="blue.300">Modi.</Box>
-        <Box bg="blue.300">Esse.</Box>
-        <Box bg="blue.300">Earum?</Box>
-        <Box bg="blue.300">Aut?</Box>
-        <Box bg="blue.300">Cumque!</Box>
-        <Box bg="blue.300">Dolore!</Box>
-        <Box bg="blue.300">Culpa.</Box>
-        <Box bg="blue.300">Veniam?</Box>
-        <Box bg="blue.300">Earum.</Box>
-        <Box bg="blue.300">Impedit!</Box>
-        <Box bg="blue.300">Sapiente!</Box>
-        <Box bg="blue.300">Sapiente?</Box>
-        <Box bg="blue.300">Voluptas?</Box>
-        <Box bg="blue.300">Illo.</Box>
-        <Box bg="blue.300">Ipsam!</Box>
-        <Box bg="blue.300">Quasi.</Box>
-        <Box bg="blue.300">Obcaecati?</Box>
-        <Box bg="blue.300">Similique.</Box>
-        <Box bg="blue.300">Totam.</Box>
-        <Box bg="blue.300">Earum.</Box>
-        <Box bg="blue.300">Temporibus?</Box>
+      <SimpleGrid gap={3} columns={{ base: 1, md: 3, xl: 6 }}>
+
+        <Card>
+          <CardBody>
+            Aspernatur consectetur hic id nobis? A cum cumque doloribus libero
+            perspiciatis reiciendis similique veniam voluptas.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Architecto consequatur culpa deleniti error fuga libero
+            necessitatibus nostrum nulla quisquam rem sed, sit, velit.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Animi asperiores autem, commodi eaque eos eum, incidunt ipsa iure
+            nobis numquam optio suscipit temporibus.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Earum explicabo harum hic illo mollitia necessitatibus porro
+            repellendus voluptas! Delectus modi nemo placeat temporibus!
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Aperiam asperiores assumenda consectetur delectus dolores est illum
+            natus nihil quo unde, veniam vitae voluptatum.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Aperiam atque, deserunt error et eum inventore ipsam neque numquam
+            provident quaerat, quas, vero voluptas?
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Alias aspernatur beatae, doloribus facilis, inventore ipsa, ipsum
+            iusto non sapiente tempore velit veritatis voluptatibus.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Cum cupiditate, id illo iure magnam magni maiores non, nulla odio
+            optio, quo veniam vitae.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Cupiditate deserunt eius expedita fuga incidunt, laudantium modi
+            quasi reiciendis repellendus rerum, sed sit ut?
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Amet beatae consectetur cupiditate deleniti dolore expedita
+            laboriosam laborum nesciunt quis rem similique tenetur, totam!
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            In iusto magni quia repellendus repudiandae. Consequatur dignissimos
+            doloremque eius fuga nemo optio repudiandae tempora!
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Ab aut, dolor eius facilis illo laborum nostrum porro quas qui
+            ratione. Accusantium, sunt veritatis.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Aspernatur at distinctio, dolorem dolores doloribus ipsum minus
+            natus, nihil odit quae ratione recusandae voluptates!
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Adipisci animi aperiam asperiores assumenda at excepturi molestias
+            nesciunt nobis recusandae soluta. Consequuntur, corporis veritatis.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Distinctio eveniet excepturi fuga id impedit modi, nam nisi odio
+            quis quos ratione sint totam.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Debitis dolores earum eos explicabo incidunt ipsa libero minus nihil
+            officia perspiciatis tenetur, vel voluptate.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Ab animi aperiam, doloremque esse, eveniet excepturi labore laborum
+            necessitatibus sequi sit ut veniam voluptate!
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Deleniti, eum impedit magnam mollitia obcaecati, officiis quasi quia
+            quod sapiente sequi soluta suscipit, vero?
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Aliquam, distinctio dolore doloribus exercitationem magnam maiores
+            nobis omnis, perferendis porro quae sunt, temporibus vel.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Commodi earum harum labore nisi numquam optio praesentium tempora
+            voluptate! Asperiores cumque incidunt nemo quasi?
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Debitis delectus, exercitationem hic molestiae nobis odit ratione
+            sed tempora. Facere laborum necessitatibus provident quo.
+          </CardBody>
+        </Card>
+        <Card>
+          <CardBody>
+            Accusantium asperiores, debitis in inventore laborum laudantium
+            magnam mollitia, pariatur quae quam reiciendis rem, voluptatibus.
+          </CardBody>
+        </Card>
       </SimpleGrid>
     </>
   );

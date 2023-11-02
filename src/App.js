9cdb11bea8e588104926492e547903990b271816
Seunggyo <tmdry99@gmail.com>
2023-11-02 16:31:16 +0900

    간단한 navbar 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import { Box, Image } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <>
-      <Box boxSize={"md"}>
-        <Image src="https://bit.ly/dan-abramov"></Image>
-      </Box>
-      <Box boxSize={"200px"}>
-        <Image src="https://bit.ly/dan-abramov" />
-      </Box>
-      <Box>
-        <Image src="https://bit.ly/dan-abramov" borderRadius={"full"}></Image>
-      </Box>
-      <Box>
-        <Image
-          src="abc.jpg"
-          fallbackSrc="https://via.placeholder.com/500"
-          borderRadius={"full"}
-        ></Image>
-      </Box>
+      <Flex
+        px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
+        justify={"space-between"}
+        py={"8px"}
+        my={"10px"}
+        bg={"gray.100"}
+      >
+        <Flex gap={"7px"} alignItems={"center"}>
+          <Text fontWeight={"extrabold"} fontSize={"xl"}>
+            NAVBAR
+          </Text>
+          <Button variant={"ghost"}>Home</Button>
+          <Button variant={"ghost"}>Link</Button>
+          <Menu>
+            <MenuButton
+              variant={"ghost"}
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+            >
+              DropDown
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+          <Button isDisabled variant={"ghost"}>
+            Disabled
+          </Button>
+        </Flex>
+        <Flex gap={"7px"} alignItems={"center"}>
+          <Input size={"md"} bgColor={"ghostwhite"}></Input>
+          <Button size={"md"} variant={"outline"} colorScheme="twitter">
+            Search
+          </Button>
+        </Flex>
+      </Flex>
     </>
   );
 }

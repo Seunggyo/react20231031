3bc2ad9a0f0ebfed1b51ae4d4a9e96d7e45312b5
Seunggyo <tmdry99@gmail.com>
2023-11-02 14:35:36 +0900

    button 의 여러가지 활용방법 (colorScheme, variant, isLoading, loadingText)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "제목",
-            description: "내용입니다.",
-            status: "success",
-            duration: 3000,
-            isClosable: true,
-            position: "top",
-          })
-        }
-      >
-        토스트 보이기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Lorem.</Button>
+      <Button colorScheme="yellow">Similique.</Button>
+      <Button colorScheme="orange">Accusamus!</Button>
+      <Button colorScheme="facebook">Fuga!</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Magnam?
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "제목",
-            description: "부정",
-            status: "error",
-            duration: 3000,
-            isClosable: true,
-            position: "top",
-          })
-        }
-      >
-        부정 보이기
+      <Button colorScheme="twitter" size={"lg"}>
+        Omnis!
       </Button>
+      <Button colorScheme="blackAlpha" variant={"outline"}>
+        Placeat!
+      </Button>
+      <Button colorScheme="whatsapp" variant={"ghost"}>
+        Tempora!
+      </Button>
+      <Button variant={"link"}>Corporis.</Button>
+      <Button leftIcon={<EmailIcon />}></Button>
+      <Button rightIcon={<ArrowRightIcon />}>Modi.</Button>
+      <Button isLoading>Asperiores!</Button>
+      <Button isLoading loadingText={"확인중"}>
+        Nostrum?
+      </Button>
+      <Button>Laudantium?</Button>
+      <Button>Veniam?</Button>
+      <Button>Dignissimos.</Button>
+      <Button>Quas?</Button>
+      <Button>Atque.</Button>
+      <Button>Quos.</Button>
+      <Button>Doloribus.</Button>
+      <Button>Ratione.</Button>
+      <Button>Nam!</Button>
+      <Button>Laborum!</Button>
+      <Button>Ex.</Button>
+      <Button>Aliquid.</Button>
+      <Button>Earum.</Button>
+      <Button>Quidem.</Button>
+      <Button>Enim!</Button>
+      <Button>Voluptas.</Button>
+      <Button>Iste.</Button>
     </>
   );
 }

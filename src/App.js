d606512b96677bc4735031be726d0e5f5ec4128c
Seunggyo <tmdry99@gmail.com>
2023-11-02 15:16:18 +0900

    select 활용하기

--- src/App.js
+++ src/App.js
@@ -1,59 +1,18 @@
-import {
-  Box,
-  Button,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, Button, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input></Input>
-            <FormHelperText>이름을 적어주세요</FormHelperText>
-            <Button>전송</Button>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={4}>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel></FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Lorem.</Radio>
-                <Radio value="value4">Lorem.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center mt={4}>
-        <Box w={"480px"}>
-          <FormControl>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="twitter">Lorem.</Checkbox>
-                <Checkbox colorScheme="twitter">Lorem.</Checkbox>
-                <Checkbox colorScheme="twitter">Lorem.</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
+      <Box w={"480px"}>
+        <Select placeholder="옵션을 선택하세요">
+          <option value="value-1">Lorem.</option>
+          <option value="value-2">Modi.</option>
+          <option value="value-3">Earum.</option>
+          <option value="value-4">Praesentium!</option>
+          <option value="value-5">Animi!</option>
+        </Select>
+        <Button colorScheme="telegram">선택</Button>
+      </Box>
     </>
   );
 }

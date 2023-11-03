40eee4b5fe9f48f987caca97378949c34996a77b
Seunggyo <tmdry99@gmail.com>
2023-11-03 09:34:25 +0900

    children prop 과 chakra ui의 응용방법

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Box, Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <Box as="a" href={to}>
+      <Button>{children}</Button>
+    </Box>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to={"https://www.naver.com"}>naver 로</MyLink>
+      <MyLink to={"https://www.daum.net"}>daum 으로</MyLink>
+    </>
+  );
 }
 
 export default App;

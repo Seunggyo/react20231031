e7bacb2818ca21712dbf13d5df8504457b9209df
Seunggyo <tmdry99@gmail.com>
2023-11-03 12:23:54 +0900

    event.preventDefault(): 기본 기능 취소

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { Button, Input } from "@chakra-ui/react";
+function App() {
+  function handleClick(e) {
+    // 브라우저가 해야하는 기본 기능을 취소함
+    e.preventDefault();
+    console.log("다른일을 시킴");
+  }
 
-function App(props) {
-  // 브라우저는 event handler method에
-  // event 객체를 매개값으로 넣어줌
-  let text = "initial text";
-
-  // 변수값이 변경되어도 바로 dom에는 반영되지 않음
-  function handleChange(e) {
-    console.log(e.target.value);
-    text = e.target.value;
+  function handleSubmit(e) {
+    // 기본기능 취소
+    e.preventDefault();
+    console.log("다른일을 시킴");
   }
 
   return (
     <>
-      <p>{text}</p>
-      <Input onChange={handleChange} />
-      <Button onClick={handleChange}>Button2</Button>
+      <a href="https://www.naver.com" onClick={handleClick}>
+        네이버
+      </a>
+      <div>
+        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
+          <input type="text" name="q" />
+          <button>검색</button>
+        </form>
+      </div>
     </>
   );
 }

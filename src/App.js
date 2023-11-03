1c89ed5ffc18a590c07593bb878f15cdb65bce53
Seunggyo <tmdry99@gmail.com>
2023-11-03 12:33:26 +0900

    event.preventDefault(): 기본 기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    e.preventDefault();
+    alert("네이버로 이동합니다.");
+    window.location.href = "https://www.naver.com";
+  }
+
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;

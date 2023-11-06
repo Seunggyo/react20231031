61aec77b6beceb7136093444237a6f1035818db1
Seunggyo <tmdry99@gmail.com>
2023-11-06 15:34:36 +0900

    필요없는 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
-import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
-import { useState } from "react";
-
 function App() {
-  const [foods, setFoods] = useState([]);
-
-  function handleButtonClick(food) {
-    setFoods([...foods, food]);
-  }
-
-  function handleRemoveButtonClick(index) {
-    // console.log(index);
-    // const nextFoods = [...foods];
-    // nextFoods.splice(index, 1);
-    // const nextFoods = foods.filter((item,i) => i! index);
-    // setFoods(nextFoods);
-
-    setFoods(foods.filter((item, i) => i != index));
-  }
 
   return (
     <>
-      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
-      <Button onClick={() => handleButtonClick("아이스크림")}>
-        아이스크림
-      </Button>
-      <Button onClick={() => handleButtonClick("케잌")}>케잌</Button>
-      <Box>
-        <UnorderedList>
-          {foods.map((item, index) => (
-            <ListItem key={index}>
-              {item}
-              <Button onClick={() => handleRemoveButtonClick(index)}>
-                지우기
-              </Button>
-            </ListItem>
-          ))}
-        </UnorderedList>
-      </Box>
+
     </>
   );
 }

cce9afd1f825015049fb7416821b693b53ae5714
Seunggyo <tmdry99@gmail.com>
2023-11-06 12:22:47 +0900

    배열을 jsx에서 사용하기, 리스트아이템에 매핑되는 컴포넌트는 key prop을 꼭 가질것
    key prop은 유일한 값이어야하며
    주로 db의 primary key가 사용 되어야한다.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ function App() {
   const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
   const arr2 = ["손흥민", "이강인", "김민재"];
   const arr3 = ["라떼", "에스프레소", "카푸치노"];
+  const arr4 = [
+    { id: 0, name: "라떼" },
+    { id: 1, name: "에스프레소" },
+    { id: 2, name: "카푸치노" },
+  ];
 
-  const listItem = arr.map((item) => <ListItem>{item}</ListItem>);
-  const listItem2 = arr2.map((item) => <ListItem>{item}</ListItem>);
+  const listItem = arr.map((item, index) => (
+    <ListItem key={index}>{item}</ListItem>
+  ));
+  const listItem2 = arr2.map((item, index) => (
+    <ListItem key={index}>{item}</ListItem>
+  ));
   return (
     <>
       <List>{listItem}</List>
       <List>{listItem2}</List>
       <Box mb={3}>
         <List>
-          {arr3.map((e) => (
-            <ListItem>{e}</ListItem>
+          {arr3.map((e, index) => (
+            <ListItem key={index}>{e}</ListItem>
           ))}
         </List>
       </Box>

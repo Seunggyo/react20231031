c311565eee686b27b1d70b3f8e234bb07b8e3ba7
Seunggyo <tmdry99@gmail.com>
2023-11-06 15:14:37 +0900

    state가 배열이면 새 state는 기존배열 복사후 새 배열로 사용 연습 시작 후 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { Box, Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick() {
+    // const newItems = [...items];
+    // newItems.push(text);
+    // setItems(newItems);
+    setItems([...items, text]);
+  }
+
+  function handleRemoveButtonClick(index) {
+    setItems(items.filter((item, i) => i != index));
+  }
+
+  return (
+    <>
+      <Box>
+        <Input value={text} onChange={(e) => setText(e.target.value)} />
+        <Button onClick={handleButtonClick}>추가</Button>
+      </Box>
+      <Box>
+        <UnorderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>
+              {item}
+              <Button
+                colorScheme="red"
+                onClick={() => handleRemoveButtonClick(index)}
+              >
+                지우기
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;

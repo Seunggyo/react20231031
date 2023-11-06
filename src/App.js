56541b9a1c35e2945e46a2f4a3359d1639b2109d
Seunggyo <tmdry99@gmail.com>
2023-11-06 11:54:41 +0900

    배열을 jsx에서 사용하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { useState } from "react";
-import { Box, Input, Text } from "@chakra-ui/react";
+import { Box, List, ListItem } from "@chakra-ui/react";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [obj, setObj] = useState({ message: "" });
+  const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
+  const arr2 = ["손흥민", "이강인", "김민재"];
+  const arr3 = ["라떼", "에스프레소", "카푸치노"];
 
-  function handleObjectMessageChange(e) {
-    // const newObj = { ...obj };
-    // newObj.message = e.target.value;
-    // setObj(newObj);
-    setObj({ ...obj, message: e.target.value });
-  }
-
+  const listItem = arr.map((item) => <ListItem>{item}</ListItem>);
+  const listItem2 = arr2.map((item) => <ListItem>{item}</ListItem>);
   return (
     <>
-      <Box>
-        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
-        <Text>{message}</Text>
-      </Box>
-      <Box>
-        <Input value={obj.message} onChange={handleObjectMessageChange} />
-        <Text>{obj.message}</Text>
+      <List>{listItem}</List>
+      <List>{listItem2}</List>
+      <Box mb={3}>
+        <List>
+          {arr3.map((e) => (
+            <ListItem>{e}</ListItem>
+          ))}
+        </List>
       </Box>
     </>
   );

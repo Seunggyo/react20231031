8d510b797b9585d6c94b1a5db09f23b409c0f6dc
Seunggyo <tmdry99@gmail.com>
2023-11-06 16:25:39 +0900

    use immer로 깊은 복사 하는 코드 없이 상태 변경

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
 import { useImmer } from "use-immer";
+import { Input, Text } from "@chakra-ui/react";
 
 function App() {
-  const [person, setPerson] = useState({ name: "son", city: "seoul" });
-  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+  const [person, updatePerson] = useImmer({
+    name: "son",
+    address: { city: "seoul", country: "korea" },
+  });
 
-  function handleNameChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.name = e.target.value;
-
-    setPerson(nextPerson);
-  }
-
-  function handleCityChange(e) {
-    const nextPerson = { ...person };
-    nextPerson.city = e.target.value;
-    setPerson(nextPerson);
-  }
-
-  function handleUserNameChange(e) {
-    updateUser((draft) => {
+  function handleChangeName(e) {
+    updatePerson((draft) => {
       draft.name = e.target.value;
     });
   }
 
-  function handleUserCityChange(e) {
-    updateUser((draft) => {
-      draft.city = e.target.value;
+  function handleChangeCity(e) {
+    updatePerson((draft) => {
+      draft.address.city = e.target.value;
+    });
+  }
+
+  function handleChangeCountry(e) {
+    updatePerson((draft) => {
+      draft.address.country = e.target.value;
     });
   }
 
   return (
     <>
-      <Input value={person.name} onChange={handleNameChange} />
-      <Input value={person.city} onChange={handleCityChange} />
+      <Input value={person.name} onChange={handleChangeName} />
+      <Input value={person.address.country} onChange={handleChangeCountry} />
+      <Input value={person.address.city} onChange={handleChangeCity} />
       <Text>
-        {person.name}은 {person.city}에 삽니다.
-      </Text>
-
-      <hr />
-      <Input value={user.name} onChange={handleUserNameChange} />
-      <Input value={user.city} onChange={handleUserCityChange} />
-      <Text>
-        {user.name}은 {user.city}에 삽니다.
+        {person.name}은 {person.address.country}, {person.address.city}에 산다
       </Text>
     </>
   );

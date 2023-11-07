3dcff79f22ac3cad87f1cbb567cea865a605d2db
Seunggyo <tmdry99@gmail.com>
2023-11-07 15:49:51 +0900

    axios 활용해서 data 가져오기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import { useEffect, useState } from "react";
-import { Button } from "@chakra-ui/react";
+import axios from "axios";
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  console.log("useEffect 밖의 로그", number);
-  // useEffect
-  // 컴포넌트 외부시스템과 작업할 때
-  // 주로 ajax로 데이터 가져올 때
-
-  // 첫번째 파라미터 : 실행할 함수
-  // 두번째 파라미터 : 첫번째 파라미터를 실행 시키는 값 , 빈 배열이면 초기 렌더링 때만 실행됨
+  const [myState, setMyState] = useState("");
   useEffect(() => {
-    console.log("이펙트의 첫번째 파라미터 함수 실행 됨", number);
+    axios
+      .get("/api/main1/sub1")
+      .then((response) => response.data)
+      .then((data) => setMyState(data))
+      .catch((error) => console.log(error));
   }, []);
-  return (
-    <>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-    </>
-  );
+  return <>{myState}</>;
 }
 
 export default App;

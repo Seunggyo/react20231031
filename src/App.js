c5077838d98eb3f28d77e737f10d06f38381f58b
Seunggyo <tmdry99@gmail.com>
2023-11-07 16:26:14 +0900

    axios get 사용 state 업데이트 연습 예제 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <></>;
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMessage(data))
+      .catch((error) => console.log(error));
+  }, []);
+  return <>{message}</>;
 }
 
 export default App;

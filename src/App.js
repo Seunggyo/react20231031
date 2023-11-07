40f8bc94ba9aa87eb272afbd12e0097a327d547c
Seunggyo <tmdry99@gmail.com>
2023-11-07 14:34:51 +0900

    context value에 {state, setState} 넣어 보기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-function App() {
+import { Input, Text } from "@chakra-ui/react";
+import { createContext, useContext, useState } from "react";
 
+function MyInput() {
+  const value = useContext(StringContext);
+  return <Input onChange={(e) => value.setString(e.target.value)} />;
+}
+
+function MyText() {
+  const value = useContext(StringContext);
+  return <Text>메시지 출력할 위치 : {value.string}</Text>;
+}
+
+function App() {
+  const [string, setString] = useState("");
   return (
     <>
-
+      <StringContext.Provider value={{ string, setString }}>
+        <MyInput />
+        <MyText />
+      </StringContext.Provider>
     </>
   );
 }
+
+const StringContext = createContext(null);
 
 export default App;

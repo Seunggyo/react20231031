8f350bfaed7e19f438b1c816b79d1ffd62276c68
Seunggyo <tmdry99@gmail.com>
2023-11-07 11:36:04 +0900

    lift state up, prop drilling 예제

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
-function MyComp2({ message }) {
+function ChildComp({ onClick }) {
   return (
-    <Box>
-      <Text>{message}</Text>
-    </Box>
+    <Button colorScheme="red" onClick={onClick}>
+      클릭
+    </Button>
   );
 }
 
-function MyComp1({ onClick }) {
-  return (
-    <Box>
-      <Button onClick={onClick}>텍스트 바꾸기</Button>
-    </Box>
-  );
+function SomeComp({ onClick }) {
+  return <ChildComp onClick={onClick} />;
+}
+
+function OtherChildComp({ message }) {
+  return <Text>{message}</Text>;
+}
+
+function OtherComp({ message }) {
+  return <OtherChildComp message={message} />;
 }
 
 function App() {
-  const [message, setMessage] = useState("hello");
-
-  function handleButtonClick() {
-    setMessage("안녕하세요");
-  }
+  const [message, setMessage] = useState("원래 메시지");
 
   return (
     <>
-      <MyComp1 onClick={handleButtonClick}></MyComp1>
-      {/*My Comp1 에 있는 버튼이 클릭되면*/}
-      <MyComp2 message={message}></MyComp2>
-      {/*  My Comp2 에 있는 텍스트가 변경*/}
+      <SomeComp onClick={() => setMessage("다른메시지")} />
+      <OtherComp message={message} />
     </>
   );
 }

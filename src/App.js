69ba5b89641102893ac2a4031d7d3696ed3e458d
Seunggyo <tmdry99@gmail.com>
2023-11-07 11:15:34 +0900

    다른컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용해야함 연습 예제 arrow function으로 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-function App() {
+import { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+
+function MyInput({ onChange, address }) {
+  return (
+    <Box>
+      <Input onChange={(e) => onChange(e.target.value)} value={address} />
+    </Box>
+  );
+}
+
+function MyText({ address }) {
+  return (
+    <Box>
+      <Text>{address}</Text>
+    </Box>
+  );
+}
+
+function App(props) {
+  const [address, setAddress] = useState("");
 
   return (
     <>
-
+      <MyInput address={address} onChange={(text) => setAddress(text)} />
+      <MyText address={address} />
     </>
   );
 }

3681897edc61f116d1705d0008d43b836b90057c
Seunggyo <tmdry99@gmail.com>
2023-11-07 14:38:57 +0900

    다른파일에 있는 compopnet가 Context 사용하기 위해서는 context는 export 해야함

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
-import { createContext, useContext, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
-
-function AComp() {
-  // 3. context 사용하기 : useContext(context)
-
-  const { setMessage } = useContext(MessageContext);
-
-  return <Button onClick={() => setMessage("변경된 메시지")}>변경 버튼</Button>;
-}
-
-function BComp() {
-  // 3. context 사용하기 useContext(Context)
-
-  const { message } = useContext(MessageContext);
-
-  return <Text>{message}</Text>;
-}
+import { createContext, useState } from "react";
+import { AComp } from "./AComp";
+import { BComp } from "./BComp";
 
 function App() {
   const [message, setMessage] = useState("초기 상태");
@@ -32,5 +17,5 @@ function App() {
 }
 
 // 1. context 만들기 : createContext(null)
-const MessageContext = createContext(null);
+export const MessageContext = createContext(null);
 export default App;

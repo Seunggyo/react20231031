67cfe0bcf475954e037e9519effc974badffefa2
Seunggyo <tmdry99@gmail.com>
2023-11-07 15:19:06 +0900

    필요없는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { useState } from "react";
-import { Button } from "@chakra-ui/react";
-
-let out = 0;
-
-function MyComp() {
-  const [num, setNum] = useState(0);
-  let a = 3;
-
-  out++;
-  console.log("a", a);
-  console.log("num", num);
-  console.log("out", out);
-  return (
-    <div>
-      <Button onClick={() => setNum(num + 1)}>증가</Button>
-      <p>{num}</p>
-    </div>
-  );
-}
-
 function App() {
-  return (
-    <>
-      <MyComp />
-    </>
-  );
+  return <></>;
 }
 
 export default App;

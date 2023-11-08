a1118c314cfb45100f4727e49af8136e92db05d1
Seunggyo <tmdry99@gmail.com>
2023-11-08 10:24:50 +0900

    useeffect 예제 로딩중일 경우와 조회한 고객이 없을경우의 상황을 만듬

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Box, Select, Text } from "@chakra-ui/react";
+import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
   const [customerId, setCustomerId] = useState(0);
-  const [cName, setCName] = useState("");
+  const [cName, setCName] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("/api/main1/sub4?id=" + customerId)
       .then((response) => response.data)
       .then((data) => setCName(data))
-      .catch((error) => console.log(error))
-      .finally(() => console.log("끝"));
+      .catch((error) => {
+        setCName(null);
+      })
+      .finally(() => setIsLoading(false));
   }, [customerId]);
   return (
     <>
@@ -31,7 +35,16 @@ function App() {
         <option value="10">10</option>
       </Select>
       <Box>
-        <Text>고객 이름 : {cName.customerName}</Text>
+        {isLoading && <Spinner />}
+
+        {isLoading || (
+          <>
+            {cName === null && (
+              <Text>조회한 고객이 없습니다. 다른번호를 선택해주세요</Text>
+            )}
+            {cName !== null && <Text>고객 이름 : {cName.customerName}</Text>}
+          </>
+        )}
       </Box>
     </>
   );

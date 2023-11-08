378e7827dd6db5272e45a527d60a901c8dd0370d
Seunggyo <tmdry99@gmail.com>
2023-11-08 14:53:28 +0900

    react-router예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Box>홈페이지</Box>} />
+      <Route path="/path1" element={<Box>경로1</Box>} />
+      <Route path="/path2" element={<Box>경로2</Box>} />
+      <Route path="/path3" element={<Box>경로3</Box>} />
+    </>,
+  ),
+);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={routes} />;
 }
 
 export default App;

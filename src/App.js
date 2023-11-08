8081496031923c42f1f27cc0e91f87cff15b0a18
Seunggyo <tmdry99@gmail.com>
2023-11-08 11:32:08 +0900

    useeffect 연습문제 연습 완료 직원 번호를 선택하면 직원의 이름이 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Box,
+  NumberInput,
+  NumberInputField,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  const [eId, setEId] = useState(0);
+  const [employee, setEmployee] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("/api/main1/sub5?id=" + eId)
+      .then((Response) => Response.data)
+      .then((data) => setEmployee(data))
+      .catch((e) => setEmployee(null))
+      .finally(() => setIsLoading(false));
+  }, [eId]);
+  // 직원 번호를 선택하면 직원의 이름이 출력
+  // /api/main1/sub5?id=
+  // spring boot의 메소드도 작성하기
+
+  let textContent = null;
+
+  if (eId === 0 || eId === null) {
+    textContent = null;
+  } else {
+    if (employee === null) {
+      textContent = (
+        <Text>해당 직원의 번호가 없습니다. 다른 번호를 입력해보세요</Text>
+      );
+    } else {
+      textContent = (
+        <Text>
+          해당 직원의 이름 : {employee.lastName} {employee.firstName}
+        </Text>
+      );
+    }
+  }
+
+  return (
+    <>
+      <Box>
+        <NumberInput min={1}>
+          <NumberInputField
+            placeholder="직원의 번호를 입력해주세요"
+            onChange={(e) => setEId(e.target.value)}
+          />
+        </NumberInput>
+        <Box>
+          {isLoading && <Spinner />}
+          {isLoading || textContent}
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default App;

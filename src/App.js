62ce766304855c340e58a52160437eb83e49cef3
Seunggyo <tmdry99@gmail.com>
2023-11-08 09:10:33 +0900

    axios get 사용 state 업데이트 연습 예제 완료 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState({});
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data))
+      .then((data) => setCustomer(data))
       .catch((error) => console.log(error));
   }, []);
-  return <>{message}</>;
+  return (
+    <>
+      <h1>Customer id : {customer.id}</h1>
+      <h1>Customer name : {customer.customerName}</h1>
+      <h1>Contract name :{customer.contactName}</h1>
+      <h1>Customer address : {customer.address}</h1>
+      <h1>Customer city : {customer.city}</h1>
+      <h1>Customer postal code : {customer.postalCode}</h1>
+      <h1>Customer country : {customer.country}</h1>
+    </>
+  );
 }
 
 export default App;
